4335fe3525b4c0d757c4914435085fc4452cb328
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-12-18 15:40:41 +0100

    fixing bugs

--- app/Http/Controllers/DiagnosisController.php
+++ app/Http/Controllers/DiagnosisController.php
@@ -5,26 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-// function getAuth(){
-//     $api_key = config('services.apmedic.api_key');
-//     $secret_key = config('services.apmedic.secret_key');
-
-//     $computedHash = base64_encode(hash_hmac ( 'md5' , 'https://authservice.priaid.ch/login' , $secret_key, true ));
-// 	$authorization = 'Bearer '.$api_key.':'.$computedHash;
-//     $r = Http::withHeaders([
-//         'Content-Type'=> 'application/json',
-//         'Authorization' => $authorization
-//     ])
-//     ->post("https://authservice.priaid.ch/login");
-//         return ($r['Token']); 
-// }
-
-class DiagnosisController extends Controller
-{
-    
-    //
-    public function index(){
-        $api_key = config('services.apmedic.api_key');
+function getAuth(){
+    $api_key = config('services.apmedic.api_key');
     $secret_key = config('services.apmedic.secret_key');
 
     $computedHash = base64_encode(hash_hmac ( 'md5' , 'https://authservice.priaid.ch/login' , $secret_key, true ));
@@ -33,12 +15,23 @@ class DiagnosisController extends Controller
         'Content-Type'=> 'application/json',
         'Authorization' => $authorization
     ])
-    ->post("https://authservice.priaid.ch/login")['Token'];
-        $ACCESS_TOKEN = $r;
+    ->post("https://authservice.priaid.ch/login")
+    -> json();
+        return $r['Token']; 
+}
+
+class DiagnosisController extends Controller
+{
+    
+    //
+    public function index(){
+        
+        $ACCESS_TOKEN = getAuth();
 
         $symptoms = Http::get("https://healthservice.priaid.ch/symptoms?token=$ACCESS_TOKEN&format=json&language=en-gb")
         ->json();
-        return view('index', ['symptoms'=> $symptoms]);
+        return $ACCESS_TOKEN;
+        // return view('index', ['symptoms'=> $symptoms]);
     }
 
     public function result(){
